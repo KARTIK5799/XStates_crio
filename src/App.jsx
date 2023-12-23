64f0af4aa8985a921019a23321f0a1106d93f9d8
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-23 19:59:02 +0530

    citySelector

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import CitySelector from './components/CitySelector'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div>
+        <CitySelector/>
+       </div>
+    </>
+  )
+}
+
+export default App
